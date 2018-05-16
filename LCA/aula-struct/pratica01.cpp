f6ce851f96cb51e45652704a8015eeb357c42725
Arthur Ribeiro <devarthurribeiro@gmail.com>
2018-05-16 15:02:27 -0300

    add struct child

--- LCA/aula-struct/pratica01.cpp
+++ LCA/aula-struct/pratica01.cpp
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+struct crianca {
+  char nome[60];
+  int dia;
+  int mes;
+  int ano;
+  char pai[60];
+  char mae[60];
+  char sexo;
+  char rua[60];
+  int numero;
+  char bairro[60];
+  char cidade[60];
+  int ddd;
+  int fone;
+};
+
 int main() {
   
   return 0;
