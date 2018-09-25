b4a53f6403cc81496a9a713c82e4f9af90619141
Arthur Ribeiro <devarthurribeiro@gmail.com>
2018-09-25 15:15:16 -0300

    pagina inicial finalizada

---
+++ Monitoria/projetc-c/main.cpp
@@ -0,0 +1,7 @@
+#include "mystring.cpp"
+
+int main() {
+  char s[20];
+  strlwr(s, 1);
+  return 0;
+}
---
+++ Monitoria/projetc-c/mystring.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "mystring.h"
+
+void strlwr(char s[], int comp) {
+   printf("strlwr\n");
+}
